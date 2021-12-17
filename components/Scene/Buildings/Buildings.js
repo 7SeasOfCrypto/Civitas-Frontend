fe18f07737835ced38bd7b4d16d2d8b57fe2f94d
sub722 <jhswebdeb@gmail.com>
2021-12-18 00:45:06 +0100

    fixed coordinates

--- components/Scene/Buildings/Buildings.js
+++ components/Scene/Buildings/Buildings.js
@@ -4,27 +4,16 @@ import {CELL_SIZE} from 'constants'
 
 const Buildings = () => {
     const { listBuild } = useStore()
-    
     const ArrayBuild= listBuild.map((value,index)=> {
-
         const {x,y,Id,id_model,level,completed}=value
         const {id,maxlevel,complete,build}=models[id_model]
-        //console.log(models[id_model])
-        
         const modelData= completed? complete[level]:build[level]
         const {size,model,colored} =modelData
         const Model=model
-        return (<Model  position={[size*CELL_SIZE/2,0,size*CELL_SIZE/2]}key={index} />)
-        return <></>
-        //listBuild:[{x:0,y:0,Id:1,id_model:1,level:0,complete:true}],
+        return (<Model  position={[(x+size/2)*CELL_SIZE,0,(y+size/2)*CELL_SIZE]} key={index} />)
     })
-    
-    
-    
-    
-    
     return(ArrayBuild)
-        
+
 }
 
 export default Buildings
--- store/Store.js
+++ store/Store.js
@@ -1,6 +1,12 @@
 import create from 'zustand'
 export const useStore = create(set => ({
-  listBuild:[{x:0,y:0,Id:1,id_model:1,level:0,completed:true}],
+  listBuild:[
+    {x:0,y:0,Id:1,id_model:1,level:0,completed:true},
+    {x:4,y:3,Id:2,id_model:1,level:0,completed:true},
+    {x:0,y:3,Id:3,id_model:1,level:0,completed:true},
+    
+  ],
   increasePopulation: () => set(state => ({ bears: state.bears + 1 })),
   removeAllBears: () => set({ bears: 0 })
 }))
+
