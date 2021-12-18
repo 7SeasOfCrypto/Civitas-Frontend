feaa4683d04f85cbea50a5171737b9ea4af86018
Arturo Iván Artaza <aartazaesc@gmail.com>
2021-12-18 17:37:01 -0300

    Added Orthographic Camera

--- components/Stage/Stage.jsx
+++ components/Stage/Stage.jsx
@@ -1,12 +1,28 @@
 
 import { Canvas } from '@react-three/fiber'
+import {MapControls} from '@react-three/drei'
+import {MOUSE} from 'three'
 import Scene from '@/components/Scene'
 
 const Stage = () => {
     return (
-        <Canvas>
+        <Canvas 
+        camera={{
+            position:[-106,81,-68],
+            rotation:[-2.27,-0.78, -2.44],
+            zoom:30,
+            near: 0,
+            far: 500,
+
+        }}
+        orthographic={true}>
            <directionalLight position={[-5, 5, 5]} castShadow shadow-mapSize-width={1024} shadow-mapSize-height={1024} />
             <Scene></Scene>
+            <MapControls
+            enableZoom={true}
+            minPolarAngle={1}
+            maxPolarAngle={1}
+            />
         </Canvas>
     );
 };
