18665871d05d178ecd8cb09a1e85a2afee0ad377
sub722 <jhswebdeb@gmail.com>
2021-12-21 21:14:52 +0100

    Fixed drop building

--- components/Scene/Buildings/Buildings.js
+++ components/Scene/Buildings/Buildings.js
@@ -4,12 +4,14 @@ import {CELL_SIZE} from 'constants'
 
 const Buildings = () => {
     const { listBuild } = useStore()
+    
     const ArrayBuild= listBuild.map((value,index)=> {
         const {x,y,Id,id_model,level,completed}=value
         const {id,maxlevel,complete,build}=models[id_model]
         const modelData= completed? complete[level]:build[level]
         const {size,model,colored} =modelData
         const Model=model
+
         return (<Model  position={[(x+size/2)*CELL_SIZE,0,(y+size/2)*CELL_SIZE]} key={index} />)
     })
     return(ArrayBuild)
