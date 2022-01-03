8081a061a1c6dd03418ad2696b86183bcaff7372
sub722 <jhswebdeb@gmail.com>
2022-01-03 21:40:07 +0100

    change load textures

--- components/Scene/Ground/Ground.js
+++ components/Scene/Ground/Ground.js
@@ -13,60 +13,32 @@ const colors = [0x006CFF, 0xFF0000, 0x2AFF00, 0x00FFCD, 0xFF00E8, 0xFFC100, 0xA2
 
 const Ground = ({ capture = false, setHover }) => {
 
-  const [textures, setTextures] = useState([])
-
   const { map, mapMaterial } = useStore(state => state.maps)
   const maps = useStore(state => state.maps)
+  const textures = useLoader(THREE.TextureLoader, ['/Textures/grass.webp', '/Textures/grass.webp', '/Textures/grass.webp', '/Textures/grass.webp', '/Textures/grass.webp', '/Textures/grass.webp', '/Textures/grass.webp'])
 
+  for (let x = 0; x < textures.length; x++) {
+    textures[x].wrapS = textures[x].wrapT = THREE.RepeatWrapping
+    if (x === 0) {
+      textures[x].repeat.set(20, 20)
+    }
 
-  const [difuse, grass] = useLoader(THREE.TextureLoader, ['/Textures/floorTile.webp', '/Textures/Grass.webp'])
- 
-  difuse.wrapS = difuse.wrapT = THREE.RepeatWrapping
-  difuse.repeat.set(.5, .5)
+  }
 
-  useEffect(() => {
-      const Textures = colors.map((value, index) => {
-        if (index !== 0) {
-          const newTexture = difuse.clone()
-          
-        
-          newTexture.repeat.set(1 / 8, 1.2)
-          newTexture.offset.set(index / 8, .1)
-          newTexture.needsUpdate = true
-          return newTexture
-        }
-        const newTexture = grass
-        grass.wrapS = grass.wrapT =THREE.RepeatWrapping
-        grass.repeat.set(25,25)
-        grass.needsUpdate = true
-    })
-    
-    
-    
-    setTextures(Textures)
-
-  }, [difuse,grass])
-
-  const materials = colors.map((value, index) =>{ 
-    const newMat=new THREE.MeshPhysicalMaterial({ map:index!==0? textures[index]:grass,precision:"highp" })
-  
-    return newMat
-})
-
-
+  const materials = colors.map((value, index) => new THREE.MeshPhysicalMaterial({ map: textures[index] }) )
   const GroundTile = mapMaterial.map((value, index) => index !== 0 ? <InstancedFloor key={index} matMap={value} material={materials[index]} index={index} /> : null)
   return (
     <>
       <group renderOrder={1}>
-        <mesh position={[CELL_SIZE*25,0,CELL_SIZE*25]} material={materials[0]}>
+        <mesh position={[CELL_SIZE * 25, 0, CELL_SIZE * 25]} material={materials[0]}>
 
-          <boxGeometry args={[CELL_SIZE * 50, .5  , CELL_SIZE * 50]}>
+          <boxGeometry args={[CELL_SIZE * 50, .5, CELL_SIZE * 50]}>
           </boxGeometry>
-          
+
 
         </mesh>
         {textures.length !== 0 ? GroundTile : null}
-        
+
       </group>
     </>
   )
--- store/Store.js
+++ store/Store.js
@@ -14,8 +14,10 @@ const enterAddMode=(set)=>{
 
 
 export const useStore = create(set => ({
-<<<<<<< HEAD
-  listBuild:[],
+  listBuild:[
+    {x:4,y:1,Id:2,id_model:0,completed:0},
+    {x:8,y:1,Id:3,id_model:0,completed:1},
+    {x:12,y:1,Id:4,id_model:0,completed:2}],
   myBuildings:[{
     id: '0x00000',
   },
@@ -25,13 +27,7 @@ export const useStore = create(set => ({
   {
     id: '0x33333',
   }],
-=======
-  listBuild:[
-    {x:4,y:1,Id:2,id_model:0,completed:0},
-    {x:8,y:1,Id:3,id_model:0,completed:1},
-    {x:12,y:1,Id:4,id_model:0,completed:2},],
   colors:{roof:0xff0000,flag1:0x00ff00,flag2:0x0000ff},
->>>>>>> 614639bc11ee116bbd1f6b7ea07357f710573242
   maps:mapGenerator(50,50),
   placeBuilding:{isAdding:false,isPlaced:false,model:0},
   enterAddMode:()=>enterAddMode(set),
